0d0be9ffe33941c4b1b8133c7fb65901aacbe783
kekoyana <kekoyana0@gmail.com>
2021-09-25 02:11:12 +0900

    rails g scaffold user name:string memo:text

---
+++ db/migrate/20210924170209_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, index: true
+      t.text :memo
+
+      t.timestamps
+    end
+  end
+end
